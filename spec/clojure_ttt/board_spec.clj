08c6653e1e7d5a9dc049266dd10bc96f3e31d1e9
LNA <LAllen@8thLight.com>
2014-09-10 11:49:52 -0500

    Makes a move on the board

---
+++ spec/clojure_ttt/board_spec.clj
@@ -0,0 +1,12 @@
+(ns clojure_ttt.board_spec
+  (:require [speclj.core :refer :all]
+            [clojure_ttt.board  :refer :all]))
+(describe "board"
+          (it "has 9 spaces"
+              (should= [0 1 2 3 4 5 6 7 8] (board))))
+
+(describe "make-move-on"
+          (it "makes a move on the board"
+              (let [board [0 1 2 3 4 5 6 7 8]]
+              (should= ["X" 1 2 3 4 5 6 7 8]
+                       (make-move-on board 0 "X")))))
---
+++ src/clojure_ttt/board.clj
@@ -0,0 +1,12 @@
+(ns clojure_ttt.board
+  (:require
+    [clojure.string :refer [replace]]
+    [clojure.java.io :as io]
+    [clojure.set])
+  (:refer-clojure :exclude [replace]))
+
+(defn board []
+  (vec (range 9)))
+
+(defn make-move-on [board mark move]
+  (assoc board mark move))
