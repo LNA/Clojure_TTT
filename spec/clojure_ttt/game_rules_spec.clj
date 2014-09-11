4ac2fd9143f16003178a09cf9b2072a0999e9fcb
LNA <LAllen@8thLight.com>
2014-09-11 15:18:50 -0500

    Finds all winning combinations

--- spec/clojure_ttt/game_rules_spec.clj
+++ spec/clojure_ttt/game_rules_spec.clj
@@ -2,82 +2,93 @@
   (:require [speclj.core :refer :all]
             [clojure_ttt.game_rules :refer :all]))
 
-(describe "row-one-win"
+(describe "row-one-win??"
           (it "gives true for a 0 1 2 win"
               (let [board ["X" "X" "X" 3 4 5 6 7 8 ]]
                 (should= true
-                         (row-one-win board "X")))))
+                         (row-one-win? board "X")))))
 
-(describe "row-two-win"
+(describe "row-two-win?"
           (it "gives true for a 3 4 5 win"
               (let [board [0 1 2 "X" "X" "X" 6 7 8]]
                 (should= true
-                         (row-two-win board "X")))))
+                         (row-two-win? board "X")))))
 
-(describe "row-three-win"
+(describe "row-three-win?"
           (it "gives true for a 6 7 8 win"
               (let [board [0 1 2 3 4 5 "X" "X" "X"]]
                 (should= true
-                         (row-three-win board "X")))))
+                         (row-three-win? board "X")))))
 
-(describe "row-win"
+(describe "row-win?"
           (it "gives true for a 6 7 8 win"
               (let [board [0 1 2 3 4 5 "X" "X" "X"]]
                 (should= true
-                         (row-win board "X"))))
+                         (row-win? board "X"))))
           (it "gives false for a non win"
             (let [board (vec (range 9))]
               (should= false
-                       (row-win board "X")))))
+                       (row-win? board "X")))))
 
-(describe "column-one-win"
+(describe "column-one-win?"
           (it "gives true for a 0 3 6 win"
               (let [board ["X" 1 2 "X" 4 5 "X" 7 8]]
                 (should= true
-                         (column-one-win board "X")))))
+                         (column-one-win? board "X")))))
 
-(describe "column-two-win"
+(describe "column-two-win?"
           (it "gives true for a 1 4 7 win"
               (let [board [0 "X" 2  3 "X" 5 6 "X" 8]]
                 (should= true
-                         (column-two-win board "X")))))
+                         (column-two-win? board "X")))))
 
-(describe "column-three-win"
+(describe "column-three-win?"
           (it "gives true for a 2 5 8 win"
               (let [board [0 1 "X" 3 4 "X" 6 7 "X"]]
                 (should= true
-                         (column-three-win board "X")))))
+                         (column-three-win? board "X")))))
 
-(describe "column-win"
+(describe "column-win?"
           (it "gives true for a 2 5 8 win"
               (let [board [0 1 "X" 3 4 "X" 6 7 "X"]]
                 (should= true
-                         (column-win board "X"))))
+                         (column-win? board "X"))))
 
           (it "gives false for a non win"
             (let [board (vec (range 9))]
               (should= false
-                       (column-win board "X")))))
+                       (column-win? board "X")))))
 
-(describe "left-diag-win"
+(describe "left-diag-win?"
           (it "gives true for a 0 4 8 win"
               (let [board ["X" 1 2 3 "X" 5 6 7 "X"]]
                 (should= true
-                         (left-diag-win board "X")))))
+                         (left-diag-win? board "X")))))
 
-(describe "right-diag-win"
+(describe "right-diag-win?"
           (it "gives true for a 2 4 6 win"
               (let [board [0 1 "X" 3 "X" 5 "X" 7 8]]
                 (should= true
-                         (right-diag-win board "X")))))
+                         (right-diag-win? board "X")))))
 
-(describe "diag-win"
+(describe "diag-win?"
           (it "gives true for a 2 4 6 win"
               (let [board [0 1 "X" 3 "X" 5 "X" 7 8]]
                 (should= true
-                         (diag-win board "X"))))
+                         (diag-win? board "X"))))
 
           (it "gives false for a non win"
             (let [board (vec (range 9))]
               (should= false
-                       (diag-win board "X")))))
+                       (diag-win? board "X")))))
+
+(describe "winner?"
+          (it "gives true for a 2 4 6 win"
+              (let [board [0 1 "X" 3 "X" 5 "X" 7 8]]
+                (should= true
+                         (winner? board "X"))))
+
+          (it "gives false for a non win"
+            (let [board (vec (range 9))]
+              (should= false
+                       (winner? board "X")))))
--- src/clojure_ttt/game_rules.clj
+++ src/clojure_ttt/game_rules.clj
@@ -5,56 +5,63 @@
     [clojure.set])
   (:refer-clojure :exclude [replace]))
 
-(defn row-one-win [board mark]
+(defn row-one-win? [board mark]
   (every? #{mark} (subvec board 0 3)))
 
-(defn row-two-win [board mark]
+(defn row-two-win? [board mark]
   (every? #{mark} (subvec board 3 6)))
 
-(defn row-three-win [board mark]
+(defn row-three-win? [board mark]
   (every? #{mark} (subvec board 6 9)))
 
-(defn row-win [board mark]
+(defn row-win? [board mark]
   (cond
-    (row-one-win board mark) true
-    (row-two-win board mark) true
-    (row-three-win board mark) true
+    (row-one-win? board mark) true
+    (row-two-win? board mark) true
+    (row-three-win? board mark) true
     :else false))
 
-(defn column-one-win [board mark]
+(defn column-one-win? [board mark]
   (cond
     (and (= mark (board 0)) (= mark (board 3)) (= mark (board 6))) true
        :else false))
 
-(defn column-two-win [board mark]
+(defn column-two-win? [board mark]
   (cond
     (and (= mark (board 1)) (= mark (board 4)) (= mark (board 7))) true
        :else false))
 
-(defn column-three-win [board mark]
+(defn column-three-win? [board mark]
   (cond
     (and (= mark (board 2)) (= mark (board 5)) (= mark (board 8))) true
        :else false))
 
-(defn column-win [board mark]
+(defn column-win? [board mark]
   (cond
-    (column-two-win board mark) true
-    (column-two-win board mark) true
-    (column-three-win board mark) true
+    (column-two-win? board mark) true
+    (column-two-win? board mark) true
+    (column-three-win? board mark) true
     :else false))
 
-(defn left-diag-win [board mark]
+(defn left-diag-win? [board mark]
   (cond
     (and (= mark (board 0)) (= mark (board 4)) (= mark (board 8))) true
        :else false))
 
-(defn right-diag-win [board mark]
+(defn right-diag-win? [board mark]
   (cond
     (and (= mark (board 2)) (= mark (board 4)) (= mark (board 6))) true
        :else false))
 
-(defn diag-win [board mark]
+(defn diag-win? [board mark]
   (cond
-    (left-diag-win board mark) true
-    (right-diag-win board mark) true
+    (left-diag-win? board mark) true
+    (right-diag-win? board mark) true
+    :else false))
+
+(defn winner? [board mark]
+  (cond
+    (row-win? board mark) true
+    (column-win? board mark) true
+    (diag-win? board mark) true
     :else false))
