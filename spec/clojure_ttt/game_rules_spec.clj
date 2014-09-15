4467913e46b087e1cc9247bd877b9a76e4213588
LNA <LAllen@8thLight.com>
2014-09-15 10:20:59 -0500

    Cleans up tests

--- spec/clojure_ttt/game_rules_spec.clj
+++ spec/clojure_ttt/game_rules_spec.clj
@@ -2,25 +2,22 @@
   (:require [speclj.core :refer :all]
     [clojure_ttt.game_rules :refer :all]))
 
-(describe "row-one-win??"
+(describe "row-win?"
   (it "gives true for a 0 1 2 win"
     (let [board ["X" "X" "X" 3 4 5 6 7 8 ]]
       (should= true
-       (row-one-win? board "X")))))
+       (row-one-win? board "X"))))
 
-(describe "row-two-win?"
   (it "gives true for a 3 4 5 win"
     (let [board [0 1 2 "X" "X" "X" 6 7 8]]
       (should= true
-       (row-two-win? board "X")))))
+       (row-two-win? board "X"))))
 
-(describe "row-three-win?"
   (it "gives true for a 6 7 8 win"
     (let [board [0 1 2 3 4 5 "X" "X" "X"]]
       (should= true
-       (row-three-win? board "X")))))
+       (row-three-win? board "X"))))
 
-(describe "row-win?"
   (it "gives true for a 6 7 8 win"
     (let [board [0 1 2 3 4 5 "X" "X" "X"]]
       (should= true
@@ -31,25 +28,22 @@
       (should= nil
        (row-win? board "X")))))
 
-(describe "column-one-win?"
+(describe "column-win?"
   (it "gives true for a 0 3 6 win"
     (let [board ["X" 1 2 "X" 4 5 "X" 7 8]]
       (should= true
-       (column-one-win? board "X")))))
+       (column-one-win? board "X"))))
 
-(describe "column-two-win?"
   (it "gives true for a 1 4 7 win"
     (let [board [0 "X" 2  3 "X" 5 6 "X" 8]]
       (should= true
-       (column-two-win? board "X")))))
+       (column-two-win? board "X"))))
 
-(describe "column-three-win?"
   (it "gives true for a 2 5 8 win"
     (let [board [0 1 "X" 3 4 "X" 6 7 "X"]]
       (should= true
-       (column-three-win? board "X")))))
+       (column-three-win? board "X"))))
 
-(describe "column-win?"
   (it "gives true for a 2 5 8 win"
     (let [board [0 1 "X" 3 4 "X" 6 7 "X"]]
       (should= true
@@ -60,19 +54,17 @@
       (should= nil
        (column-win? board "X")))))
 
-(describe "left-diag-win?"
+(describe "diag-win?"
   (it "gives true for a 0 4 8 win"
     (let [board ["X" 1 2 3 "X" 5 6 7 "X"]]
       (should= true
-       (left-diag-win? board "X")))))
+       (left-diag-win? board "X"))))
 
-(describe "right-diag-win?"
   (it "gives true for a 2 4 6 win"
     (let [board [0 1 "X" 3 "X" 5 "X" 7 8]]
       (should= true
-       (right-diag-win? board "X")))))
+       (right-diag-win? board "X"))))
 
-(describe "diag-win?"
   (it "gives true for a 2 4 6 win"
     (let [board [0 1 "X" 3 "X" 5 "X" 7 8]]
       (should= true
