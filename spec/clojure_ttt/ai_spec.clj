7a0200a833b8b13722df24d594f4d1e618d6c878
LNA <LAllen@8thLight.com>
2014-09-17 09:59:35 -0500

    Adds dumb ai

--- spec/clojure_ttt/ai_spec.clj
+++ spec/clojure_ttt/ai_spec.clj
@@ -1,3 +1,9 @@
 (ns clojure_ttt.ai_spec
   (:require [speclj.core :refer :all]
-    [clojure_ttt.game_rules :refer :all]))
+    [clojure_ttt.ai :refer :all]))
+
+(describe "ai" 
+  (it "make an easy move"
+    (let [board ["X" 1 2 3 4 5 "X" "X" "X"]]
+      (should= 1
+       (ai-move board)))))
