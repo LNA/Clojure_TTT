4e286b958e8744dc6fa9f80d618b80283745002b
LNA <LAllen@8thLight.com>
2014-09-18 11:58:29 -0500

    Adds Mock for game over message

--- src/clojure_ttt/runner.clj
+++ src/clojure_ttt/runner.clj
@@ -17,16 +17,15 @@
           next-type next-type
           board         board]
     (if (r/game-over? board next-mark)
-      (u/game-over-message next-mark board)
+      (u/game-over-message next-mark board) 
       (do (u/ask-for-move current-mark)
-      (let [move (get-move board current-type)
-           updated-board (p/make-move-on board move current-mark)]
-       (u/print-board updated-board)
-       (recur next-mark current-mark next-type current-type updated-board))))))
+        (let [move (get-move board current-type)
+             updated-board (p/make-move-on board move current-mark)]
+          (u/print-board updated-board)
+          (recur next-mark current-mark next-type current-type updated-board))))))
 
-  (defn -main []
-    (u/welcome-message)
-    (let [_ (u/ask-for-mark "one")
+(defn start []
+  (let [_ (u/ask-for-mark "one")
           current-mark (read-line)
           _ (u/ask-for-type "one")
           current-type (read-line)
@@ -36,5 +35,9 @@
           next-type (read-line)
           board (vec (range 9))]
           (u/lets-begin-message)
-          (u/print-board board)
+          (u/print-board board) 
    (game-loop current-mark next-mark current-type next-type board)))
+
+  (defn -main []
+    (u/welcome-message)
+    (start))
