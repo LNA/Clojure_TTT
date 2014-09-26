a0e96331ba2ceb9854a1a373c03e2d909567b00e
LNA <LAllen@8thLight.com>
2014-09-26 17:39:03 -0500

    WIP

--- spec/clojure_ttt/ai_spec.clj
+++ spec/clojure_ttt/ai_spec.clj
@@ -6,13 +6,16 @@
   (it "gives the rank of a specific move"
     (let [max-winning-board ["X" "X" "X" 3 4 5 6 7 8]
           min-winning-board ["O" "O" "O" 3 4 5 6 7 8]
-          tie-board         ["X" "X" "O" "O" "O" "X" "X" "O" "O"]]
+          tie-board         ["X" "X" "O" "O" "O" "X" "X" "O" "O"]
+          in-progress-board [0  "X" 2 "O" "O" "X" "X" "O" "X"]]
       (should= 500
-        (rank max-winning-board "X" "O" 0))
+        (rank max-winning-board "X" "O"))
       (should= -500
-        (rank min-winning-board "X" "O" 0))
+        (rank min-winning-board "X" "O"))
       (should= 0
-        (rank tie-board "X" "O" 0))))
+        (rank tie-board "X" "O")
+      (should= 100
+        (rank in-progress-board "X" "O")))))
 
 (it "MINIMAX"
   (let [board [0  "X"  2 
@@ -21,4 +24,4 @@
         max-mark "X"
         min-mark "O" ]
     (should= 0
-      (minimax board max-mark min-mark 0)))))
+      (minimax board max-mark min-mark)))))
--- src/clojure_ttt/ai.clj
+++ src/clojure_ttt/ai.clj
@@ -8,21 +8,13 @@
 (defn get-open-spaces [board]
   (keep-indexed #(if (number? %2) %1) board))
 
-(defn rank-tie [board]
-  (if (r/tie? board)
-    0))
+(defn rank [board mark opponent]
+  (cond
+    (r/tie? board mark opponent) 0
+    (r/winner? board mark)       500
+    (r/winner? board opponent)  -500)
+    (r/game-in-progress? board mark opponent) 100)
 
-(defn rank-winner [board mark opponent depth] 
-  (if (r/winner? board mark)  
-    500 
-    (do 
-      (r/winner? board opponent) 
-      -500)))
-
-(defn rank [board mark opponent depth]
-  (if (r/tie? board mark opponent)
-    (+ depth 0)
-    (rank-winner board mark opponent depth)))
 
 (defn track-moves [space max-rank min-rank]
   (let [score (hash-map space, max-rank)
@@ -36,11 +28,12 @@
 ; "space min" {2 499}
 ; "space max" {2 -499} nil 
 
-(defn best-move-for [board space mark opponent depth]
-  (let [depth (+ 1 depth)
-        max-rank (- depth (rank (b/make-move-on board space mark) mark opponent depth))
-        min-rank (* -1 (+ depth (rank (b/make-move-on board space opponent) opponent mark depth)))]
-        (track-moves space max-rank min-rank)))
+(defn best-move-for [board space mark opponent]
+  (let [depth 0
+        max-rank (rank (b/make-move-on board space mark) mark opponent)
+        min-rank (rank (b/make-move-on board space opponent) opponent mark)]
+        (prn max-rank board space)))
+        ;(track-moves space max-rank min-rank)))
 
-(defn minimax [board max-mark min-mark depth]
-  (map (fn [x] (best-move-for board x max-mark min-mark depth)) (get-open-spaces board)))
+(defn minimax [board max-mark min-mark]
+  (map (fn [x] (best-move-for board x max-mark min-mark)) (get-open-spaces board)))
