62becaafab1058a5644783746c878917c3f778cc
LNA <LAllen@8thLight.com>
2014-09-26 17:08:53 -0500

    Started Over

--- src/clojure_ttt/ai.clj
+++ src/clojure_ttt/ai.clj
@@ -24,65 +24,23 @@
     (+ depth 0)
     (rank-winner board mark opponent depth)))
 
-(defn move-on-open-space [space board max-mark min-mark depth]
-  (let [next-board (b/make-move-on board space max-mark)]
-      (+ depth (rank next-board max-mark min-mark depth))))
-         
-(defn rank-open-spaces [board max-mark min-mark depth]
-  (let [spaces (get-open-spaces board)]
-  (map (fn [space] (move-on-open-space space board max-mark min-mark depth)) spaces)))
+(defn track-moves [space max-rank min-rank]
+  (let [score (hash-map space, max-rank)
+        new-score (hash-map space, min-rank)]
+        (cond 
+          (< (first (keys score)) (first (keys new-score))) new-score
+          (> (first (keys score)) (first (keys new-score))) score)))
 
-(defn build-rankings [board max-mark min-mark depth]
-  (let [open-spaces (get-open-spaces board)
-        ranks       (rank-open-spaces board max-mark min-mark depth)]
-    (zipmap ranks open-spaces)))
-
-(defn find-best-ranked-move [board max-mark min-mark depth]
-  (let [rank-graph  (build-rankings board max-mark min-mark depth)]
-    (prn "rank-graph" rank-graph)
-    (get rank-graph (key (apply max-key val rank-graph)))))
-
-(defn add-next-open-space [board current-mark next-mark]
-  (let [board board
-        move (first (get-open-spaces board))]
-        (b/make-move-on board move current-mark)))
-
-(defn return-best-move [board max-mark min-mark depth] ;should not get here if (= 1 (count open-spaces))
-  ;(prn board)
-  (let [max-move (find-best-ranked-move board max-mark min-mark depth)
-        min-move (find-best-ranked-move board min-mark max-mark depth)]
-        ;(prn "max move" max-move "board" board)
-        ;(prn "min-move" min-move "board" board) 
-        (if (<= max-move min-move)
-          max-move
-          min-move)))
-
-(defn store-move-and-score [board max-mark min-mark depth ]
-  (let [rank-graph (build-rankings board max-mark min-mark depth)]
-    (if (= {} rank-graph)
-         {}
-        (let [scores (apply max-key val rank-graph)] 
-         (into {} [scores])))))
-
-(defn next-open-space [open-spaces]
-  (first (next open-spaces))
-  )
-
-(defn return-move [stored-move]
-  (apply val stored-move))
-
-(defn find-max-score [score-hash]
-  (prn "score-hash:" (score-hash)))
-
-(defn smash-hash [space max-rank min-rank]
-  (hash-map space, max-rank)
-  (hash-map space, min-rank))
+; "space min" {0 499}
+; "space max" {0 501} nil why are these returning nil?
+; "space min" {2 499}
+; "space max" {2 -499} nil 
 
 (defn best-move-for [board space mark opponent depth]
   (let [depth (+ 1 depth)
         max-rank (- depth (rank (b/make-move-on board space mark) mark opponent depth))
         min-rank (* -1 (+ depth (rank (b/make-move-on board space opponent) opponent mark depth)))]
-        (smash-hash space max-rank min-rank)))
+        (track-moves space max-rank min-rank)))
 
 (defn minimax [board max-mark min-mark depth]
   (map (fn [x] (best-move-for board x max-mark min-mark depth)) (get-open-spaces board)))
