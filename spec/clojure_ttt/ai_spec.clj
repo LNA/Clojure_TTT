fba15f3ae464483c353e2ecefe662e9310a766fe
LNA <LAllen@8thLight.com>
2014-09-29 20:31:21 -0500

    Deletes specs for private rank method

--- spec/clojure_ttt/ai_spec.clj
+++ spec/clojure_ttt/ai_spec.clj
@@ -3,25 +3,29 @@
     [clojure_ttt.ai :refer :all]))
 
 (describe "ai"
-  (it "gives the rank of a specific move"
-    (let [max-winning-board ["X" "X" "X" 3 4 5 6 7 8]
-          min-winning-board ["O" "O" "O" 3 4 5 6 7 8]
-          tie-board         ["X" "X" "O" "O" "O" "X" "X" "O" "O"]
-          in-progress-board [0  "X" 2 "O" "O" "X" "X" "O" "X"]]
-      (should= 500
-        (rank max-winning-board "X" "O"))
-      (should= 500
-        (rank min-winning-board "O" "X"))
-      (should= 0
-        (rank tie-board "X" "O"))
-      (should= 100
-        (rank in-progress-board "X" "O")))))
+  (it "returns the best move"
+    (let [board [0  "X"  2 
+                "O" "O" "X" 
+                "X" "O" "X"]
+          max-mark "X"
+          min-mark "O" ]
+      (should= 2
+        (minimax board max-mark min-mark))))
 
-(it "MINIMAX"
-  (let [board [0  "X"  2 
-              "O" "O" "X" 
-              "X" "O" "X"]
+  (it "returns the best move"
+   (let [board ["X"  1    2 
+                "O" "X" "O" 
+                "X" "X" "X"]
         max-mark "X"
         min-mark "O" ]
-    (should= 0
+    (should= 1
       (minimax board max-mark min-mark))))
+
+  (it "returns the best move"
+    (let [board [0  "X"  2 
+                "X" "O" "O" 
+                "X" "O" "X"]
+          max-mark "X"
+          min-mark "O" ]
+      (should= 0
+        (minimax board max-mark min-mark)))))
