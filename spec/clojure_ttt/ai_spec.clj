3aeb4ddfd5cf16d664d9d3d8d885f6235f08718e
LNA <LAllen@8thLight.com>
2014-10-16 14:31:30 -0500

    Adds constants

--- spec/clojure_ttt/ai_spec.clj
+++ spec/clojure_ttt/ai_spec.clj
@@ -3,16 +3,27 @@
     [clojure_ttt.ai :refer :all]))
 
 (describe "ai"
+  (it "blocks a win"
+    (let [board [0   1 "h"
+                 "a" 4 "h"
+                  6 "a" 8]
+          max-mark "a"
+          min-mark "h"]
+          (should= 8
+            (ai-move board max min-mark))))
+
   (it "plays the correct move"
-    (let [board ["o"  1  "x"
-                  3  "x" "x"
-                  6   7  "0"]
+    (let [board ["o" 1  2
+                  3  4  5
+                  6  7  8]
           max-mark "o"
           min-mark "x"]
-          (should= 3
-            (ai-move board max-mark min-mark))))
+          (should= 4
+            (ai-move board max-mark min-mark))
+          (should= 4
+            (ai-move board min-mark max-mark))))
 
-  (xit "plays a corner on the first move"
+  (it "plays a corner on the first move"
       (let [board [0  1   2
                     3 4  5
                     6 7  8]
@@ -22,7 +33,7 @@
             (should-contain move
               [0 2 6 8])))
   
-  (xit "finds the best move"
+  (it "finds the best move"
       (let [max-board ["h" 1 2 "h" 4 5 6 "a" 8]
             min-board ["h" 1 2 3 4 5 "a" "a" 8]]
         (should= 6
@@ -30,12 +41,12 @@
         (should= 8
          (ai-move min-board "h" "a"))))
 
-   (xit "blocks a knight set up"
-      (let [board ["O"  1   2
-                    3  "X"  5
-                    6  "O"  8]
-            max-mark "X"
-            min-mark "O"
+   (it "blocks a knight set up"
+      (let [board ["h"  1   2
+                    3  "a"  5
+                    6  "h"  8]
+            max-mark "a"
+            min-mark "h"
              move (ai-move board max-mark min-mark)]
             (should-contain move
               [3 5 6 8]))))
--- src/clojure_ttt/ai.clj
+++ src/clojure_ttt/ai.clj
@@ -6,20 +6,25 @@
     [clojure_ttt.game_rules    :as r])
   (:refer-clojure :exclude [replace]))
 
+(def ^:const highest-score 500)
+(def ^:const tie-score 0)
+(def ^:const in-progress-score 100)
+(def ^:const initial-depth 0)
+
 (defn- get-open-spaces [board]
   (keep-indexed #(if (number? %2) %1) board))
 
 (defn- rank [board max-mark min-mark]
   (cond
-    (r/game-in-progress? board max-mark min-mark) 100 
-    (r/tie? board max-mark min-mark) 0
-    (r/winner? board max-mark min-mark)       500))
+    (r/game-in-progress? board max-mark min-mark) in-progress-score
+    (r/tie? board max-mark min-mark) tie-score
+    (r/winner? board max-mark min-mark)       highest-score))
 
-(defn- get-boards [board max-mark min-mark depth]
+(defn- get-boards [board max-mark min-mark] 
   (map (fn [space] (b/make-move-on board space max-mark)) (get-open-spaces board)))
 
-(defn- scores-before-depth [board max-mark min-mark depth]
-  (map (fn [board] (rank board max-mark min-mark)) (get-boards board max-mark min-mark depth))) 
+(defn- scores-before-depth [board max-mark min-mark]
+  (map (fn [board] (rank board max-mark min-mark)) (get-boards board max-mark min-mark))) 
 
 (defn- apply-depth [depth score]
   (if (odd? depth)
@@ -27,7 +32,7 @@
     (- score depth))) 
 
 (defn- get-scores [board max-mark min-mark depth]
-  (map (fn [x] (apply-depth depth x)) (scores-before-depth board max-mark min-mark depth)))
+  (map (fn [x] (apply-depth depth x)) (scores-before-depth board max-mark min-mark)))
 
 (defn- build-scores [board max-mark min-mark depth]
   (zipmap (get-open-spaces board) (get-scores board max-mark min-mark depth)))
@@ -47,32 +52,23 @@
 (defn- best-max-score [board max-mark min-mark depth]
   (last (first (sort-by val > (build-scores board max-mark min-mark depth)))))
 
-(defn minimax [board max-mark min-mark depth best-score]
+(defn minimax [board max-mark min-mark depth best-score] 
   (loop [board board
          max-mark max-mark
          min-mark min-mark
          depth depth
-         best-score best-score]
-         (prn "")
-         (prn "best-score" best-score)
-         (prn "depth" depth)
-         (prn "max score" (build-scores board max-mark min-mark depth))
-         (prn "min score" (build-scores board min-mark max-mark depth))
-         (prn "best-max-score " (best-max-score board max-mark min-mark depth))
-         (prn "best min score" (best-min-score board min-mark max-mark depth))
-         (prn "best min move"  (best-min-move board min-mark max-mark depth))
-         (prn "best max move" (best-max-move board max-mark min-mark depth))
-         (prn "")
-  (if (= best-score (best-max-score board max-mark min-mark depth))    ;if there is a win for the ai
-    (best-max-move board max-mark min-mark depth)                      ;take the win
-     (do                                                               ;else
-      (if (= best-score (best-min-score board min-mark max-mark depth));if there is a win for the opponent
-        (best-min-move board min-mark max-mark depth)                  ;block that win
-        (recur (b/make-move-on board (remaining-scores board max-mark min-mark depth) max-mark) min-mark max-mark (inc depth) (dec best-score)))))))
+         best-score best-score]                                                             
+      (if (= best-score (best-min-score board min-mark max-mark depth))
+        (best-min-move board min-mark max-mark depth)              
+        (recur 
+          (b/make-move-on board (remaining-scores board max-mark min-mark depth) max-mark) 
+          min-mark max-mark (inc depth) (dec best-score)))))
   
 (defn get-move [board max-mark min-mark depth]
-  (minimax board max-mark min-mark depth 500))
+  (if (= highest-score (best-min-score board max-mark min-mark depth))
+    (best-max-move board max-mark min-mark depth)
+    (minimax board max-mark min-mark depth highest-score)))
 
 (defn ai-move [board max-mark min-mark]
   (if (= false (r/game-over? board min-mark max-mark))
-   (get-move board max-mark min-mark 0)))
+   (get-move board max-mark min-mark initial-depth)))
